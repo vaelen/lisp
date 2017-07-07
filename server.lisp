2e0471438339459c4152121221b62324361a60e9
Andrew Young <andrew.young@mongodb.com>
2017-07-07 18:41:56 -0500

    Updated to use ql properly.

--- server.lisp
+++ server.lisp
@@ -1,4 +1,14 @@
-(require "usocket")
+
+(ql:quickload :usocket)
+(ql:quickload :usocket-server)
+
+(defpackage :server
+  (:use :common-lisp
+        :usocket)
+  (:export :start-server
+           :stop-server))
+
+(in-package :server)
 
 (defparameter *log-stream* *standard-output*)
 (defvar *server-thread* 'nil)
@@ -12,39 +22,40 @@
   (format stream "Welcome User.~C" #\newline)
   (force-output stream)
   (let ((done))
-    (loop while
-         (and (not done) (open-stream-p stream)) do
-         (format stream "> ")
-         (force-output stream)
-         (let ((line (remove #\newline (read-line stream))))
-           (cond ((equal (string-downcase line) "quit")
-                  (format stream "Goodbye!~C" #\newline)
-                  (setf done t))
-                 ((equal (string-downcase line) "time")
-                  (format stream "The current time is : ~A~%" (get-universal-time)))
-                 (t
-                  (format stream "Your command was: ~A ~%" line)))
-           (force-output stream)
-           ))))
+    (loop
+      while (and (not done) (open-stream-p stream)) do
+      (format stream "> ")
+      (force-output stream)
+      (let ((line (remove #\newline (read-line stream))))
+        (cond ((equal (string-downcase line) "quit")
+               (format stream "Goodbye!~C" #\newline)
+               (setf done t))
+          ((equal (string-downcase line) "time")
+           (format stream "The current time is : ~A~%" (get-universal-time)))
+          (t
+           (format stream "Your command was: ~A ~%" line)))
+        (force-output stream)))))
 
 (defun user-tcp-handler (stream)
   (declare (type stream stream))
-  (log-message (format 'nil "Port Opened: ~A ~A" usocket:*remote-host* usocket:*remote-port*))
+  (log-message (format 'nil "Port Opened: ~A ~A" *remote-host* *remote-port*))
   ;; TODO : Keep track of threads
   (user-repl stream)
-  (log-message (format 'nil "Port Closed: ~A ~A" usocket:*remote-host* usocket:*remote-port*)))
+  (log-message (format 'nil "Port Closed: ~A ~A" *remote-host* *remote-port*)))
 
 (defun stop-server ()
-  (cond (*server-thread*
-         (log-message (format 'nil "Shutting Down Server"))
-         (sb-thread:terminate-thread *server-thread*)
-         (log-message (format 'nil "Server Shut Down")))
-        (t (log-message (format 'nil "Server Is Not Running")))))
-         
+  (cond
+    (*server-thread*
+     (log-message (format 'nil "Shutting Down Server"))
+     (sb-thread:terminate-thread *server-thread*)
+     (log-message (format 'nil "Server Shut Down")))
+    (t (log-message (format 'nil "Server Is Not Running")))))
 
 (defun start-server (host port)
   (log-message (format 'nil  "Starting Server ~A:~A" host port))
   (setq *server-thread*
-       (usocket:socket-server host port #'user-tcp-handler '() :in-new-thread t :multi-threading t)))
-
- 
+        (socket-server host port
+                       #'user-tcp-handler
+                       '()
+                       :in-new-thread t
+                       :multi-threading t)))
